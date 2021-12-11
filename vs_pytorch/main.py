4f9ffdaf25c15cd15163c173d9b8194c2d19dad0
mohamad-amin <mohammadi.mohamadamin@gmail.com>
2021-12-10 18:24:55 -0800

    Testing kernel implementation

---
+++ vs_pytorch/main.py
@@ -0,0 +1,69 @@
+import os
+import jax
+import torch
+import time
+import random
+import shutil
+import logging
+import argparse
+import tempfile
+import numpy as np
+from jax import numpy as jnp
+from collections import OrderedDict
+from torch.utils.tensorboard import SummaryWriter
+
+import utils
+import wide_resnet
+import model_builder
+import dataloader_builder
+from vs_pytorch.ntk_computer import compute_ntk
+
+
+def main(config, logger):
+
+    model_config = config['model']
+    train_config = config['train']
+    eval_config = config['eval']
+    data_config = config['data']
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+    num_devices = torch.cuda.device_count()
+
+    dataloaders = dataloader_builder.build(config, logger)
+    models = model_builder.build(model_config, data_config, logger)
+
+    if not isinstance(models['model'], torch.nn.DataParallel):
+        if torch.cuda.device_count() > 1:
+            models['model'] = utils.DataParallel(models['model'])
+        models['model'] = models['model'].to(device)
+
+    al_params = data_config['al_params']
+    subset = dataloaders['subset']
+
+    models['ntk_params'] = utils.convert_params_to_jax(models['ntk_params'], models['model'])
+
+    labeled_dataset, unlabeled_dataset = dataloaders['train'].dataset, dataloaders['unlabeled'].dataset
+    labeled_set, subset = dataloaders['labeled_set'], dataloaders['subset']
+
+    X_train, y_train = utils.get_full_data(labeled_dataset, labeled_set)
+
+    import IPython; IPython.embed()
+    ntk_kernel = compute_ntk(X_train, models, 10, num_devices, model_config, True)
+
+
+if __name__ == '__main__':
+
+    # Fixing the pthread_cancel glitch while using python 3.8 (you can comment these two lines if you're on 3.7)
+    import ctypes
+    libgcc_s = ctypes.CDLL('libgcc_s.so.1')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', default='',  help="Path to a config")
+    args = parser.parse_args()
+
+    logger = utils.load_log(tempfile.NamedTemporaryFile().name)
+    config = utils.load_config(args.config_path)
+
+    main(config, logger)
+
---
+++ vs_pytorch/ntk_computer.py
@@ -0,0 +1,37 @@
+import time
+import numpy as np
+from jax import numpy as jnp
+from functools import partial
+
+import utils
+
+
+def ntk_fn_dynamic_batched(ntk_fn_builder, num_devices, params, max_batch_size, x1, x2=None):
+    x2a = x1 if x2 is None else x2
+    n1, n2 = x1.shape[0], x2a.shape[0]
+    assert n1 * n2 % (num_devices * num_devices) == 0, "both {} and {} should be divisible by #GPUs!".format(n1, n2)
+    optimal_batch_size = np.gcd(n1, n2) // num_devices
+    divisors = utils.get_sorted_divisors(optimal_batch_size)[::-1]
+    batch_size = divisors[divisors < max_batch_size][0]
+    print('n1 - {} n2 - {} batch size - {}'.format(n1, n2, batch_size))
+    return ntk_fn_builder(batch_size=batch_size)(x1, x2, params)
+
+
+def compute_ntk(X, models, ntk_max_batch_size, num_devices, model_config, double_precision=True):
+
+    dtype = jnp.float64 if double_precision else jnp.float32
+    X = jnp.asarray(X.detach().cpu().numpy().transpose(0, 2, 3, 1), dtype=dtype)
+
+    ntk_fn_builder = models['ntk_fn_builder']
+    ntk_fn_batched, params = models['ntk_fn'], models['ntk_params']
+    num_classes, ntk_config = model_config['model_arch']['num_classes'], model_config['ntk']
+
+    dynamic_batched_ntk_fn = partial(ntk_fn_dynamic_batched, ntk_fn_builder,
+                                     num_devices, params, ntk_max_batch_size)
+
+    prev_time = time.time()
+    kernel = dynamic_batched_ntk_fn(X, None)  # n x n x kn x kn
+    t = time.time() - prev_time
+    print('Computing took {:4f}sec'.format(t))
+
+    return kernel
